a131e0165a62b246e3b92ff3eda659cf1f01f673
Udayakumar Rayala <uday.rayala@gmail.com>
2011-10-02 22:40:27 +0100

    Added lein and a skelton main

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject myproject "20 Questions Client"
+  :description "A self learning program which tries to guess a personality with in 20 questions"
+  :url "http://github.com/uday-rayala/20QuestionsClient"
+  :main ruk.main
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ src/ruk/main.clj
@@ -0,0 +1,2 @@
+(ns ruk.main (:gen-class))
+(defn -main [& args] (println "Lets start the game!!"))
