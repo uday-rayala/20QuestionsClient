0720acba4e8b17135e424b90a54709cd3d09b1f3
Udayakumar Rayala <uday.rayala@gmail.com>
2011-10-03 18:25:27 +0100

    Refactored

--- test/ruk/twenty_questions_test.clj
+++ test/ruk/twenty_questions_test.clj
@@ -3,33 +3,33 @@
   (:use midje.sweet)
   (:require [clojure.zip :as zip]))
 
-(def dr-who-question-bank (question-zip {:question "Are you a character from Dr Who?" :yes "Dr Who" :no "Jimmy Carter"}))
-(def question-bank (question-zip {:question "Are you a character from Dr Who?"
+(def dr-who-question-bank {:question "Are you a character from Dr Who?" :yes "Dr Who" :no "Jimmy Carter"})
+(def question-bank {:question "Are you a character from Dr Who?"
                            :yes "Dr Who"
                            :no {:question "Are you a famous athlete?"
                                 :yes {:question "Are you a sprinter?"
                                       :yes "Usain Bolt"
                                       :no "Michael Phelps"}
                                 :no "Jimmy Carter"}
-                           }))
+                           })
 
 (defn answer? [answer] (fn [x] (= answer (zip/node x))))
-(fact (identify dr-who-question-bank) => (answer? "Dr Who")
+(fact (identify (question-zip dr-who-question-bank)) => (answer? "Dr Who")
   (provided (ask "Are you a character from Dr Who?") => "Yes"))
 
-(fact (identify dr-who-question-bank) => (answer? "Jimmy Carter")
+(fact (identify (question-zip dr-who-question-bank)) => (answer? "Jimmy Carter")
   (provided (ask "Are you a character from Dr Who?") => "No"))
 
-(fact (identify question-bank) => (answer? "Usain Bolt")
+(fact (identify (question-zip question-bank)) => (answer? "Usain Bolt")
   (provided (ask "Are you a character from Dr Who?") => "No"
             (ask "Are you a famous athlete?") => "Yes"
             (ask "Are you a sprinter?") => "Yes"))
 
-(fact (identify question-bank) => (answer? "Jimmy Carter")
+(fact (identify (question-zip question-bank)) => (answer? "Jimmy Carter")
   (provided (ask "Are you a character from Dr Who?") => "No"
             (ask "Are you a famous athlete?") => "No"))
 
-(fact (identify question-bank) => (answer? "Michael Phelps")
+(fact (identify (question-zip question-bank)) => (answer? "Michael Phelps")
   (provided (ask "Are you a character from Dr Who?") => "No"
             (ask "Are you a famous athlete?") => "Yes"
             (ask "Are you a sprinter?") => "No"))
